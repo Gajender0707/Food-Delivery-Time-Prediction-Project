b862417937335641cf4f6dfb1f66a1381884b475
Gajender <Iamsanju0707@gmail.com>
2024-09-09 17:00:05 +0530

    adding the database

--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,22 @@
 from exception import CustomException
 from logger import logging
+import pandas as pd
+import sqlite3
+import pandas as pd
 
 
-try:
-    a+b 
-    logging.info("adding the files")
 
-except Exception as e:
-    raise CustomException(e)
+
+# Create a connection to the SQLite database (it will create the database if it doesn't exist)
+conn = sqlite3.connect('my_database.db')
+
+
+df=pd.read_csv("Artifacts/train.csv")
+# print(df)
+
+df.to_sql("my_table",conn,if_exists="replace",index=False)
+
+
+db_from_db=pd.read_sql("Select * from my_table",conn)
+
+print(db_from_db)
