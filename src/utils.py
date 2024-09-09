7dd0452f1a49c144253c4aa095e49cd6c1ab611c
Gajender <Iamsanju0707@gmail.com>
2024-09-09 17:17:44 +0530

    ading the files

--- src/utils.py
+++ src/utils.py
@@ -3,20 +3,30 @@ from logger import logging
 import pandas as pd
 import sqlite3
 import pandas as pd
-
-
+import os 
 
 
 # Create a connection to the SQLite database (it will create the database if it doesn't exist)
 conn = sqlite3.connect('my_database.db')
 
+class data_from_db:
 
-df=pd.read_csv("Artifacts/train.csv")
-# print(df)
-
-df.to_sql("my_table",conn,if_exists="replace",index=False)
+    def __init__(self,row_data_path):
+        self.row_data_path=row_data_path
 
 
-db_from_db=pd.read_sql("Select * from my_table",conn)
+    def data_store(self):
+        df=pd.read_csv(self.row_data_path)
+        # print(df)
+        df.to_sql("training_data",conn,if_exists="append",index=False)
 
-print(db_from_db)
+
+
+
+    def data(self):
+        pass
+
+
+if __name__=="__main__":
+    obj=data_from_db("data/train.csv")
+    obj.data_store()
